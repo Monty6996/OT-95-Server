ce5f9c89db8fcccec660a7f2a986871a269c87bb
Manuel Montañana <51271465+Monty6996@users.noreply.github.com>
2021-11-29 16:11:56 -0300

    Update contact-middleware.js

--- app/middlewares/contact-middleware.js
+++ app/middlewares/contact-middleware.js
@@ -7,7 +7,7 @@ module.exports = {
     check('name', 'required').notEmpty(),
     check('email', 'required').notEmpty().bail(),
     check('email', 'invalid email').isEmail().bail(),
-    check('email', 'invalid email').custom(helper.contactExist),
+    //check('email', 'invalid email').custom(helper.contactExist),
     check('phone', 'invalid phone').optional().isLength({ min: 8, max: 11 }),
     validateFields
   ]
